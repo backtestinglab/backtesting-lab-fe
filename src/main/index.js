4af4dca9297b222f3d3d2147a5cc179357155c5b
David Nowak <bigair_1080@hotmail.com>
2025-06-07 00:28:21 +0300

    update Electron window config

--- src/main/index.js
+++ src/main/index.js
@@ -6,10 +6,11 @@ import icon from '../../resources/icon.png?asset'
 function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 900,
-    height: 670,
+    width: 1440,
+    height: 900,
     show: false,
     autoHideMenuBar: true,
+    title: 'BacktestingLab',
     ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
